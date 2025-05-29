f90cbb9ac896c4a2fe83206681e5ed4bf5be42d0
Vito6161 <vidros_77@protonmail.com>
2025-05-29 08:26:34 -0300

    pontuação corrigida
    AA

--- Pinball/Assets/scripts/bola.cs
+++ Pinball/Assets/scripts/bola.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class bola : MonoBehaviour
 {
+    public int pontuaçãoTotal;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -16,10 +18,27 @@ public class bola : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D colisao)
     {
-
         if (colisao.collider.CompareTag("chão"))
         {
             Destroy(gameObject);
         }
+    }
+
+    void OnTriggerEnter2D(Collider2D colisao)
+    {
+        if (colisao.CompareTag("25 pontos"))
+        {
+            pontuaçãoTotal += 25;
+        }
+        else if (colisao.CompareTag("50 pontos"))
+        {
+            pontuaçãoTotal += 50;
+        }
+        else if (colisao.CompareTag("100 pontos"))
+        {
+            pontuaçãoTotal += 100;
+        }
+
+        Debug.Log(pontuaçãoTotal);
     }
 }
